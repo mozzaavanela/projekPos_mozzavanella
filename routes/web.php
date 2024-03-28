35007287a0e8627a1b797cb8302fdf1e9c7a9ca6
mozzaavanela <ojanella9@gmail.com>
2024-03-28 11:08:03 +0700

    membuat user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,5 @@ Route::resource('kategori',CategoryController::class)
 route::resource('pelanggan',CustomerController::class)->except('destroy');
 
 Route::resource('produk', ProductController::class);
+
+Route::resource('pengguna',UserController::class)->except('destroy','create','show','update','edit');
